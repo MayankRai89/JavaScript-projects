9ff2f891614ac867d6052468f1a75e1cc2845c15
Mayank Rai <149506720+MayankRai89@users.noreply.github.com>
2026-02-04 21:06:52 +0530

    Refactor time remaining calculation to use a dedicated function for improved readability

--- DownloadingEffect/script.js
+++ DownloadingEffect/script.js
@@ -5,17 +5,26 @@ var h2 = document.querySelector("h2");
 var inner = document.querySelector(".inner");
 var h3 = document.querySelector("h3");
 
+function updateTimeRemaining(totalTime, progress) {
+  return (((100 - progress) / 100) * totalTime).toFixed(1);
+}
+
 btn.addEventListener("click", function () {
   btn.style.pointerEvents = "none";
 
   var num = 50 + Math.floor(Math.random() * 50);
   h3.innerHTML = "Time remaining - " + num / 10 + " seconds";
+
+  var totalTime = num / 10;
+
   var interval = setInterval(() => {
     grow++;
     h2.innerHTML = grow + "%";
     inner.style.width = grow + "%";
-    remainingTime = (totalTime * (100 - grow)) / 100;
-    h3.innerHTML = "Time remaining - " + remainingTime.toFixed(1) + " seconds";
+   h3.innerHTML =
+      "Time remaining - " +
+      updateTimeRemaining(totalTime, grow) +
+      " seconds";
   }, num);
 
   setTimeout(() => {
