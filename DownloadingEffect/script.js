4885cd47574bd4661f77cc311c6e73d50d36476a
Mayank Rai <149506720+MayankRai89@users.noreply.github.com>
2026-02-04 21:03:27 +0530

    Fix heading level for time remaining display and update script to reflect changes

--- DownloadingEffect/script.js
+++ DownloadingEffect/script.js
@@ -3,16 +3,19 @@ var grow = 0;
 var btn = document.querySelector("button");
 var h2 = document.querySelector("h2");
 var inner = document.querySelector(".inner");
+var h3 = document.querySelector("h3");
 
 btn.addEventListener("click", function () {
   btn.style.pointerEvents = "none";
 
   var num = 50 + Math.floor(Math.random() * 50);
-  console.log("Your file will be downloaded in", num / 10, "seconds");
+  h3.innerHTML = "Time remaining - " + num / 10 + " seconds";
   var interval = setInterval(() => {
     grow++;
     h2.innerHTML = grow + "%";
     inner.style.width = grow + "%";
+    remainingTime = (totalTime * (100 - grow)) / 100;
+    h3.innerHTML = "Time remaining - " + remainingTime.toFixed(1) + " seconds";
   }, num);
 
   setTimeout(() => {
